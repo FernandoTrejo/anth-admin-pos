7183c48d9318f39de7b355a50a429492fe04ba49
ronaldfernandotrejo@gmail.com <ronaldfernandotrejo@gmail.com>
2023-05-09 21:30:36 -0600

    proteger rutas

--- routes/api.php
+++ routes/api.php
@@ -31,63 +31,65 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-// Route::middleware('auth:sanctum')->group(function () {
-//     Route::get('/user', function (Request $request) {
-//         return $request->user();
-//     });
-// });
+
+Route::post('/login', [LoginController::class, 'authenticate'])->name('login-attempt');
+
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/login', [LoginController::class, 'authenticate'])->name('login-attempt');
-// TRASLADOS
-Route::prefix('traslados')->group(function () {
-    // Route::post('crear', [TrasladoController::class, 'CrearNuevo']);
-    Route::post('finalizar', [CambiarEstadoTrasladoController::class, 'Finalizar']);
-    Route::post('rechazar', [CambiarEstadoTrasladoController::class, 'Cancelar']);
-    Route::get('consultar_todos', [ConsultarTrasladosController::class, 'ConsultarTodos']);
-    Route::get('consultar_enviados_hacia/{claveCentroCosto}/{estado}', [ConsultarTrasladosController::class, 'ConsultarTrasladosHaciaMiSucursal']);
-    Route::get('consultar_enviados_desde/{claveCentroCosto}/{estado}', [ConsultarTrasladosController::class, 'ConsultarTrasladosHaciaMiSucursal']);
-    Route::get('consultar_ultimos_enviados_hacia/{claveCentroCosto}', [ConsultarTrasladosController::class, 'ConsultarUltimosTrasladosHaciaMiSucursal']);
-    Route::get('consultar_ultimos_modificados/{claveCentroCosto}', [ConsultarTrasladosController::class, 'ConsultarUltimosTrasladosModificados']);
-});
+Route::middleware('auth:api')->group(function () {
 
-// INVENTARIO
-Route::prefix('inventario')->group(function () {
-    Route::get('consultar_inventario_activo/{skip}/{take}', [ConsultarInventarioController::class, 'ConsultarInventarioActivo']);
-    Route::get('consultar_inventario_activo/cantidad', [ConsultarInventarioController::class, 'CantidadInventarioActivo']);
-});
 
-// Menu
-Route::prefix('menu')->group(function () {
-    Route::get('categorias_productos', [ConsultarMenuProductosController::class, 'Consultar']);
-});
+    // TRASLADOS
+    Route::prefix('traslados')->group(function () {
+        // Route::post('crear', [TrasladoController::class, 'CrearNuevo']);
+        Route::post('finalizar', [CambiarEstadoTrasladoController::class, 'Finalizar']);
+        Route::post('rechazar', [CambiarEstadoTrasladoController::class, 'Cancelar']);
+        Route::get('consultar_todos', [ConsultarTrasladosController::class, 'ConsultarTodos']);
+        Route::get('consultar_enviados_hacia/{claveCentroCosto}/{estado}', [ConsultarTrasladosController::class, 'ConsultarTrasladosHaciaMiSucursal']);
+        Route::get('consultar_enviados_desde/{claveCentroCosto}/{estado}', [ConsultarTrasladosController::class, 'ConsultarTrasladosHaciaMiSucursal']);
+        Route::get('consultar_ultimos_enviados_hacia/{claveCentroCosto}', [ConsultarTrasladosController::class, 'ConsultarUltimosTrasladosHaciaMiSucursal']);
+        Route::get('consultar_ultimos_modificados/{claveCentroCosto}', [ConsultarTrasladosController::class, 'ConsultarUltimosTrasladosModificados']);
+    });
 
-// Usuarios
-Route::prefix('usuarios')->group(function () {
-    Route::get('consultar_usuarios', [ConsultarUsuariosPOSController::class, 'Consultar']);
-});
+    // INVENTARIO
+    Route::prefix('inventario')->group(function () {
+        Route::get('consultar_inventario_activo/{skip}/{take}', [ConsultarInventarioController::class, 'ConsultarInventarioActivo']);
+        Route::get('consultar_inventario_activo/cantidad', [ConsultarInventarioController::class, 'CantidadInventarioActivo']);
+    });
 
-// Sucursales
-Route::prefix('sucursales')->group(function () {
-    Route::get('consultar_todas', [ConsultarSucursalesInfoController::class, 'ConsultarTodas']);
-    Route::get('consultar_info/{codigo}', [ConsultarSucursalesInfoController::class, 'ConsultarInfoSucursal']);
-});
+    // Menu
+    Route::prefix('menu')->group(function () {
+        Route::get('categorias_productos', [ConsultarMenuProductosController::class, 'Consultar']);
+    });
 
-//cajas
-Route::prefix('cajas')->group(function () {
-    Route::get('consultar_info/{codigo}', [ConsultarInfoCajasController::class, 'ConsultarInfoCaja']);
-});
+    // Usuarios
+    Route::prefix('usuarios')->group(function () {
+        Route::get('consultar_usuarios', [ConsultarUsuariosPOSController::class, 'Consultar']);
+    });
 
-//vendedores
-Route::prefix('vendedores')->group(function () {
-    Route::get('consultar_todos', [ConsultarVendedoresController::class, 'ConsultarTodos']);
-});
+    // Sucursales
+    Route::prefix('sucursales')->group(function () {
+        Route::get('consultar_todas', [ConsultarSucursalesInfoController::class, 'ConsultarTodas']);
+        Route::get('consultar_info/{codigo}', [ConsultarSucursalesInfoController::class, 'ConsultarInfoSucursal']);
+    });
 
-Route::prefix('sync_server')->group(function () {
-    Route::post('sync_transacciones_pendientes', [ImportarTransaccionesPendientesController::class, 'Importar']);
-    Route::post('sync_kardex_pendientes', [ImportarKardexPendienteController::class, 'Importar']);
-    Route::post('sync_cortes_pendientes', [ImportarCortesPendientesController::class, 'Importar']);
-    Route::post('sync_traslados_pendientes', [ImportarTrasladosController::class, 'Importar']);
+    //cajas
+    Route::prefix('cajas')->group(function () {
+        Route::get('consultar_info/{codigo}', [ConsultarInfoCajasController::class, 'ConsultarInfoCaja']);
+    });
+
+    //vendedores
+    Route::prefix('vendedores')->group(function () {
+        Route::get('consultar_todos', [ConsultarVendedoresController::class, 'ConsultarTodos']);
+    });
+
+    Route::prefix('sync_server')->group(function () {
+        Route::post('sync_transacciones_pendientes', [ImportarTransaccionesPendientesController::class, 'Importar']);
+        Route::post('sync_kardex_pendientes', [ImportarKardexPendienteController::class, 'Importar']);
+        Route::post('sync_cortes_pendientes', [ImportarCortesPendientesController::class, 'Importar']);
+        Route::post('sync_traslados_pendientes', [ImportarTrasladosController::class, 'Importar']);
+    });
 });
