9cb2d3d05345ba62a2bb912bcf793700664e0b13
ronaldfernandotrejo@gmail.com <ronaldfernandotrejo@gmail.com>
2023-10-20 09:38:08 -0600

    agregar info de producto en venta diaria de producto

--- app/Http/Controllers/API/Reportes/VentaDiariaProductoSucursalController.php
+++ app/Http/Controllers/API/Reportes/VentaDiariaProductoSucursalController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API\Reportes;
 
 use App\Http\Controllers\Controller;
+use App\Models\Producto;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Src\shared\APIResponse;
@@ -15,10 +16,18 @@ class VentaDiariaProductoSucursalController extends Controller
             $fechaInicio = $datos['init_date'];
             $fechaFin = $datos['final_date'];//YYYY-MM-DD
             $codigoProducto = $datos['product_code'];
+
+            $prod = Producto::where('codigo', $codigoProducto)->first();
+            if(!$prod){
+                $response = new APIResponse(404, false, 'No existe el codigo de producto solicitado', []);
+                return response()->json($response->toArray());
+            }
+
             $sucursalesLista = $datos['stores_list'];
             $ventasProducto = DB::select("CALL x_BuscarVentasPorProducto(?,?,?,?)", [$codigoProducto, $sucursalesLista, $fechaInicio, $fechaFin]);
             $response = new APIResponse(200, true, 'OK', [
-                'ventas' => $ventasProducto
+                'ventas' => $ventasProducto,
+                'producto' => $prod->toArray()
             ]);
             return response()->json($response->toArray());
         } catch (\Throwable $th) {
